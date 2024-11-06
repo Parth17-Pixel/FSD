24312175f98af7e275b50f9dc07bb876038faa9f
Parth17-Pixel <parth.22b1541156@abes.ac.in>
2024-11-06 12:00:41 +0530

    First Attempt

---
+++ Day5/script.js
@@ -0,0 +1,40 @@
+// function display(){
+//     console.log("Hello 35");
+// }
+// let x="hello";
+//  console.log(typeof x);
+// let obj={
+//     "name": "Rahul"
+//     id:12,
+// }
+// let ar=[12,2,3,4]
+// console.log(typeof ar);
+
+// function display(){
+//     console.log("hello")
+// }
+// display()
+
+
+// function validate(){
+//     let name1 = document.getElementById("name").value;
+//     let pass = document.getElementById("pass").value;
+//     if(name1 == "admin" && pass == "admin")
+//         document.getElementById("res").innerHTML = "Login Success";
+//     else
+//     document.getElementById("res").innerHTML = "Login Failed";
+// }
+
+// let a()=> {
+//     console.log("hello");
+// };
+
+// a();
+// let a= (x,y)=>{
+//     return(x+y);
+// };
+
+// let x=a(12,34);
+// console.log(x)
+
+let say
